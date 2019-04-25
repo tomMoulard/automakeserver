1c5d51066bd3acd99a324c34fa2103842598d73b
Tom Moulard <tom.moulard@epita.fr>
2019-04-25 15:59:17 +0200

    server: adding CLI options

--- server.go
+++ server.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-    "fmt"
-    "github.com/julienschmidt/httprouter"
-    "net/http"
-    "log"
     "encoding/json"
+    "fmt"
+    "github.com/akamensky/argparse"
+    "github.com/julienschmidt/httprouter"
+    "github.com/tommoulard/automakeserver/helper"
+    "log"
+    "net/http"
     "os"
     "os/exec"
-    "github.com/tommoulard/automakeserver/helper"
+    "strings"
 )
+
+var script = ""
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
     fmt.Fprint(w, "Welcome!\n")
@@ -36,7 +40,7 @@ func Update(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
             "unpublished": false,
         }
         if !actions[request.Action] {return}
-        cmd := exec.Command("/bin/sh", "script.sh", request.Action)
+        cmd := exec.Command("/bin/sh", script, request.Action)
         cmd.Stdout = os.Stdout
         cmd.Stderr = os.Stderr
         cmd.Run()
@@ -44,8 +48,23 @@ func Update(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 }
 
 func main() {
+    parser := argparse.NewParser("automakeserver","Get POST request from Github and launch scripts")
+    port := parser.String("p", "port",
+        &argparse.Options{Default: "8080", Help:"Server port"})
+    scrpt := parser.String("s", "scipt",
+        &argparse.Options{Default: "script.sh", Help:"Script to execute"})
+    err := parser.Parse(os.Args)
+    if err != nil {
+        fmt.Println(parser.Usage(err))
+        os.Exit(1)
+    }
+    script = *scrpt
+    var portBuilder strings.Builder
+    portBuilder.WriteString(":")
+    portBuilder.WriteString(*port)
+
     router := httprouter.New()
     router.GET("/", Index)
     router.POST("/update/", Update)
-    log.Fatal(http.ListenAndServe(":8080", router))
+    log.Fatal(http.ListenAndServe(portBuilder.String(), router))
 }
